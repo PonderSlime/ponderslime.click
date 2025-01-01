6a59cbd11fb80442cb9d17a4630b1dac95431271
PonderSlime (PC) <micah.j.edwards1@gmail.com>
2025-01-01 07:50:00 -0700

    Fixed header links

--- ponderslime.click/components/header.tsx
+++ ponderslime.click/components/header.tsx
@@ -5,7 +5,7 @@ const GlobalHeader = () => {
     return (
         <header className="w-full h-full">
             <div className="row-start-1 flex gap-12 flex-wrap items-center justify-center">
-                <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="#">
+                <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/">
                 <FontAwesomeIcon icon={faUser} className="fa-fw" />
                 About Me
                 </a>
